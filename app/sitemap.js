67d56b472303dc4d53c4c2c1298a97e747332364
sajjad <sajjad.pd.1384@gmail.com>
2026-07-24 11:59:12 +0330

    sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://simonteam.ir',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://simonteam.ir/aboutUs',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://simonteam.ir/contactUs',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://simonteam.ir/khadamat',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://simonteam.ir/packs',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://simonteam.ir/portfolios',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
